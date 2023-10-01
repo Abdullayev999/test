3e69c021ce0846dcbd95bfedd1aab88e981ea984
Abdullayev999 <fabdullayev902@gmail.com>
2023-10-01 15:46:15 +0400

    update

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,15 +1,4 @@
 public class Main {
-<<<<<<< HEAD
-
-
-
-    public  void test(){
-
-    }
-
-
-=======
->>>>>>> 9a30e71 (update)
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
