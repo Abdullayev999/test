5dd8296d1c77fbd2d1f1dbddd7297fb36197d84d
Abdullayev999 <fabdullayev902@gmail.com>
2023-10-01 15:56:35 +0400

    sozdal

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,20 +1,3 @@
 public class Main {
 
-    public  void addFunc(){
-
-    }
-
-    // master
-    //prodaction
-    //developer
-    //test
-
-
-//     sl,s[ ,[s [s ]]]
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-    }
 }
