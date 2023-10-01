cf2e80864a7cd30dc5424bd582c6123355861c06
Abdullayev999 <fabdullayev902@gmail.com>
2023-10-01 15:48:13 +0400

    dobavil komment

--- test/src/Main.java
+++ test/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public  void addFunc(){
 
     }
+
+
+
+//     sl,s[ ,[s [s ]]]
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
